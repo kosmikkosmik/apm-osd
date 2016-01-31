0ab2084c931667e0ab5149c9a6d975ae8088de6f
Konstantin Romanov <kosmik@outlook.com>
2016-01-31 21:38:01 +0300

    Missing file

---
+++ ArduCAM_OSD/Battery.h
@@ -0,0 +1,43 @@
+// Battery.h
+
+#ifndef _BATTERY_h
+#define _BATTERY_h
+
+#include <FastSerial.h>
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "ParameterManager.h"
+
+class BatteryClass
+{
+protected:
+    int8_t    m_batteryPercentage;
+    uint16_t  m_dischargeRate;
+    float     m_voltage;
+    ParameterManagerClass*  m_pParameterManager;
+
+public:
+    void init(ParameterManagerClass* pParameterManager);
+
+    void SetBatteryPercentage(int8_t percentage);
+    void SetDischargeRate(float rate);
+    void SetVoltage(float voltage);
+    uint16_t GetTotalCapacity() const;
+    uint16_t GetFailsafeCapacity() const;
+    uint16_t GetRemainingCapacity() const;
+
+    unsigned long GetRemainingTime() const;
+    float GetVoltage() const { return m_voltage; }
+    uint16_t GetBatteryPercentage() const { return m_batteryPercentage; }
+    uint16_t GetDischargeRate() const { return m_dischargeRate; }
+};
+
+extern BatteryClass Battery;
+
+#endif
+
